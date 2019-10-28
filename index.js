542beb4dbef71038534a2f0d55c8335d08950188
kh45 <khaledhassan45@gmail.com>
2019-10-28 16:58:34 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(seasons) {
+  let season = seasons.find(function(season) {
+    return season.result === "W";
+  });
+  if (season === undefined) {
+    return undefined
+  } else {
+    return season.year;
+  }
+};
