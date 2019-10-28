f0e37b0ea6dc7024ba78a3bb4d26db088db3ddcf
nihaprezz <sergio13prez@gmail.com>
2019-10-28 17:04:24 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(record) {
+  let result = record.find(function(e){
+     return e.result === "W"
+  })
+
+  if (result === undefined) {
+    return undefined
+  } else {
+    return result.year;
+  }
+}
